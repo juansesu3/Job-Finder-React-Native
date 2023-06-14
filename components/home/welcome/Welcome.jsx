da4c12b5d8ca06d7545beb668f8486dec6467578
juansesu3 <juanseu3@hotmail.com>
2023-06-14 10:53:09 +0200

    still working on fonst and used the similar useEffect hook was called useCallback

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Jaun</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
